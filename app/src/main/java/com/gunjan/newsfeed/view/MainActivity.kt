549280e194fabd27540122be604f824600243332
Gunjan Shakya <gunjan.shakya3@gmail.com>
2023-04-04 11:02:45 +0545

    First commit

---
+++ app/src/main/java/com/gunjan/newsfeed/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gunjan.newsfeed.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gunjan.newsfeed.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
