cf47d9f1ca6e68e48256aab9a6be3d8e6d7ffd59
Gunjan Shakya <gunjan.shakya3@gmail.com>
2023-04-05 15:37:08 +0545

    Login design completed

--- app/src/main/java/com/gunjan/newsfeed/view/LoginFragment.kt
+++ app/src/main/java/com/gunjan/newsfeed/view/LoginFragment.kt
@@ -6,13 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gunjan.newsfeed.R
+import com.gunjan.newsfeed.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
