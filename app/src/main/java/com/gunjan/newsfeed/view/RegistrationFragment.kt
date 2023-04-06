741d098d7c9a8623347fd507867f7170d82c7e75
Gunjan Shakya <gunjan.shakya3@gmail.com>
2023-04-06 07:05:33 +0545

    Registration form validation

--- app/src/main/java/com/gunjan/newsfeed/view/RegistrationFragment.kt
+++ app/src/main/java/com/gunjan/newsfeed/view/RegistrationFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.gunjan.newsfeed.R
 import com.gunjan.newsfeed.core.utils.NavigationRedirection
+import com.gunjan.newsfeed.core.utils.UiText
 import com.gunjan.newsfeed.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
@@ -21,19 +23,77 @@ class RegistrationFragment : Fragment() {
         _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
 
         binding.btnNext.setOnClickListener {
-            val actionExploreToProductList =
-                RegistrationFragmentDirections.actionRegisterToSetPassword(
-                    binding.textInputEditTextFullName.text.toString(),
-                    binding.textInputEditTextPhone.text.toString(),
-                    binding.textInputEditTextAddress.text.toString(),
-                    binding.textInputEditTextEmail.text.toString()
-                )
-            binding.root.findNavController().navigate(actionExploreToProductList)
+            if (isFullNameValid() and isPhoneValid() and isAddressValid() and isEmailValid()) {
+                binding.apply {
+                    val actionExploreToProductList =
+                        RegistrationFragmentDirections.actionRegisterToSetPassword(
+                            textInputEditTextFullName.text.toString(),
+                            textInputEditTextPhone.text.toString(),
+                            textInputEditTextAddress.text.toString(),
+                            textInputEditTextEmail.text.toString()
+                        )
+                    binding.root.findNavController().navigate(actionExploreToProductList)
+                }
+            }
         }
 
         binding.btnBack.setOnClickListener { NavigationRedirection.navigateBack(it) }
 
         return binding.root
+    }
+
+    private fun isFullNameValid(): Boolean {
+        binding.textInputEditTextFullName.apply {
+            return if (text.isNullOrBlank()) {
+                error = UiText.StringResource(R.string.required).asString(requireContext())
+                false
+            } else {
+                error = null
+                true
+            }
+        }
+    }
+
+    private fun isPhoneValid(): Boolean {
+        binding.textInputEditTextPhone.apply {
+            return if (text.isNullOrBlank()) {
+                error = UiText.StringResource(R.string.required).asString(requireContext())
+                false
+            } else if (text!!.length < 10) {
+                error = UiText.StringResource(R.string.invalid_number).asString(requireContext())
+                false
+            } else {
+                error = null
+                true
+            }
+        }
+    }
+
+    private fun isAddressValid(): Boolean {
+        binding.textInputEditTextAddress.apply {
+            return if (text.isNullOrBlank()) {
+                error = UiText.StringResource(R.string.required).asString(requireContext())
+                false
+            } else {
+                error = null
+                true
+            }
+        }
+    }
+
+    private fun isEmailValid(): Boolean {
+        binding.textInputEditTextEmail.apply {
+            return if (text.isNullOrBlank()) {
+                error = UiText.StringResource(R.string.required).asString(requireContext())
+                false
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(text.toString()).matches()) {
+                error = UiText.StringResource(R.string.invalid_email).asString(requireContext())
+                false
+            } else {
+                error = null
+                true
+            }
+        }
     }
 
     override fun onDestroyView() {
